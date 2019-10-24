feb95efcc3fff603e80b4cd39eebf297f8fd9b17
Camilla <cks@live.se>
2019-10-24 23:22:33 +0200

    Updated content (data.php) and added .css-file

--- data.php
+++ data.php
@@ -1,33 +1,103 @@
 <?php
 
 /**
- * All data like arrays and articles
+ * Data containing authors and articles
  * 
  */
 
-
-$news = [
+$authors = [
     [
     'id' => 1, 
-    'title' => 'Something nice', 
-    'content' => 'Something well written',
-    'date' => 20191023,
-    'likes' => 3,
+    'fullName' => 'Richard Hendrick'
     ],
     [
     'id' => 2, 
-    'title' => 'hello2', 
-    'content' => 'Something well written',
-    'date' => 20191024,
-    'likes' => 6,
+    'fullName' => 'Erlich Bachman'
     ],
-
+    [
+    'id' => 3, 
+    'fullName' => 'Nelson "Big Head" Bihetti'
+    ],
+    [
+    'id' => 4, 
+    'fullName' => 'Bertram Gilfoyle'
+    ],
+    [
+    'id' => 5, 
+    'fullName' => 'Dinesh Chugtai'
+    ],
 ];
 
-$authors = [
-     ['id' => 1, 'fullName' => 'Mickey Mouse'],
-     ['id' => 2, 'fullName' => 'Donald Duck'],
-     ['id' => 3, 'fullName' => 'Goofy Goof'],
-     ['id' => 4, 'fullName' => 'Winnie the Pooh'],
-     ['id' => 5, 'fullName' => 'Thomas O\'Malley'],
+$articles = [
+    [
+    'author' => $authors[0]['fullName'],
+    'title' => 'Tech Evangelist', 
+    'content' => 'Despite all our differences, we all have one thing in common, right? A signed agreement with me to build and launch your websites, not on the tired, bloated, old web, but on the new Internet of the future. The Pipernet. Which is why, from this day forward, I\'m going to be naming the eight of you the OctoPipers. (Everybody frowns) Until something better...',
+    'date' => '2019-10-23',
+    'likes' => 11,
+    ],
+    [
+    'id' => $authors[0]['fullName'], 
+    'title' => 'Minimum Viable Product', 
+    'content' => 'Look, guys, for thousands of years, guys like us gave gotten the shit kicked out of us. But now, for the first time, we\'re living in an era, where we can be in charge and build empires. We could be the Vikings of our day.',
+    'date' => '2019-10-24',
+    'likes' => 23,
+    ],
+    [
+    'id' => $authors[1]['fullName'], 
+    'title' => 'Hooli-Con', 
+    'content' => 'Every time we\'ve gotten a whiff of success, a giant pelican by the name of Fate takes a four-and-a-half-pound shit right on top of us.',
+    'date' => '2019-10-24',
+    'likes' => 39,
+    ],
+    [
+    'id' => $authors[1]['fullName'], 
+    'title' => 'Bachman\'s Earning\'s Over-Ride ', 
+    'content' => 'What do you think would happen if I didn\'t show up to this "Vanity Fair" event? I\'ve already RSVP\'d and given them a list of phony dietary restrictions just to cause a scene. I told them I was pesca-pescatarian. Which is one who eats solely fish who eat other fish. I think my absence would be noted.',
+    'date' => '2019-10-24',
+    'likes' => 14,
+    ],
+    [
+    'id' => $authors[2]['fullName'], 
+    'title' => 'Bachmanity Insanity', 
+    'content' => 'Yeah, turns out the guy who built this place knew exactly what he was doing, and the pool was right where it needed to be the whole time. But now I know that for sure, which feels good.',
+    'date' => '2019-10-24',
+    'likes' => 42,
+    ],
+    [
+    'id' => $authors[2]['fullName'], 
+    'title' => 'Tech Evangelist', 
+    'content' => 'Yeah, thanks! The Monterey Bay Aquarium was testing their sonar, and found this thing, like, two miles under the ocean. And I figure, Erlich and I already bought it, so I might as well hold onto it. Plus, it weighs like 8,000 pounds, so I couldn\'t move it if I wanted to.',
+    'date' => '2019-10-24',
+    'likes' => 9,
+    ],
+    [
+    'id' => $authors[3]['fullName'], 
+    'title' => 'Reorientation', 
+    'content' => 'What we saw was a very oily man in mid-sentence dip down, vomit, and then thrust himself violently face first into a glass wall. But I guess it\'s a lot less embarrassing the way you explain it.',
+    'date' => '2019-10-24',
+    'likes' => 17,
+    ],
+    [
+    'id' => $authors[3]['fullName'], 
+    'title' => 'Bachman\'s Earning\'s Over-Ride', 
+    'content' => 'Let me put this in terms you\'ll understand. I\'m like a suicide bomber of humiliation. I\'m happy to go out as long as I take you with me. Your shame is my paradise.',
+    'date' => '2019-10-24',
+    'likes' => 51,
+    ],
+    [
+    'id' => $authors[4]['fullName'], 
+    'title' => 'Artificial Emotional Intelligence', 
+    'content' => 'As your supervisor, I wanted to inform you that there is a first aid kit, but if you need blood, don\'t go to Gilfoyle \'cause he\'s type-O. Typo! They love it. They\'re all laughing at you.',
+    'date' => '2019-10-24',
+    'likes' => 29,
+    ],
+    [
+    'id' => $authors[4]['fullName'], 
+    'title' => 'The Keenan Vortex', 
+    'content' => 'I walked up a hill, and on top of the hill, there was a tavern. And I walked into the tavern and I could just look everywhere, and there was a barmaid. Guess what. I walked right up to the barmaid, and I just looked at her for a really long time.',
+    'date' => '2019-10-24',
+    'likes' => 53,
+    ],
 ];
+
--- index.php
+++ index.php
@@ -5,16 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="style.css"> 
-    <title>A catchy title soon to be added</title>
+    <link rel="stylesheet" href="style.css">
+    <style>@import url('https://fonts.googleapis.com/css?family=Cormorant+Garamond|Old+Standard+TT&display=swap');</style> 
+    <title>The Silicon Valley Post</title>
 </head>
 <body>
     
 <?php require __DIR__.'/data.php' ?>
 
-<?php foreach ($authors as $author) : ?>
+<?php foreach ($articles as $article) : ?>
 
-<h3><?php echo $author['fullName']; ?></h3>
+<h2><?php echo $article['title']; ?></h2>
+<p><?php echo $article['content']; ?></p>
+<p><?php echo $article['date']; ?></p>
+
 
 <?php endforeach; ?>
 
