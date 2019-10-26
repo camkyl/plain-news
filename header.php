6086150957cc284d4f3c775627bc599a1647ad86
Camilla <cks@live.se>
2019-10-26 12:44:37 +0200

    Created header

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="style.css">
+    <style>@import url('https://fonts.googleapis.com/css?family=Cormorant+Garamond|Nunito+Sans|Old+Standard+TT&display=swap');</style> 
+    <title>News of Silicon Valley</title>
+</head>
+<body>
+    <div>
+    <header>
+        <h1>News of Silicon Valley</h1>
+    </header>
+
