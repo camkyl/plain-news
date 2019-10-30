a437f3d583d71ce6efa548980cbdb4c05d3942dc
Camilla <cks@live.se>
2019-10-30 16:37:05 +0100

    Functions added
    Sort by date and random likes

--- functions.php
+++ functions.php
@@ -1,10 +1,35 @@
 <?php
 
-/**
- * All functions.
- * 
- */
-
 declare(strict_types=1);
 
 
+/**
+ * Sorting post by latest date. New to old. 
+ * 
+ * @param array $a
+ * @param array $b
+ * 
+ * @return bool
+ */
+
+
+function sortByLatestDate (array $a, array $b): bool {
+    return strtotime($a['publishDate']) < strtotime($b['publishDate']); // strtotime() changes a given date string into timestamp
+}
+
+usort($articles, 'sortByLatestDate'); // sort array with given user-defined function
+
+
+/**
+ * Generating a random number of likes for each article.
+ * 
+ * @param int $numberOfLikes
+ * 
+ * @return int
+ */
+
+function randomLikes(): int {
+    $numberOfLikes = rand(0,200);
+
+    return $numberOfLikes;
+}
